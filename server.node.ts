d3be93a02d8c4073d02933d7a702ab7e7b15688b
Jeff See <jeffsee.55@gmail.com>
2026-04-23 11:58:29 -0700

    Bump

--- server.node.ts
+++ server.node.ts
@@ -3,7 +3,7 @@ import Express from "express";
 const app = Express();
 
 app.use("/", (_req, res) => {
-  res.send("Hello from Express with Nitro!");
+  res.send("Hello from Express with Nitro");
 });
 
 export default app;
